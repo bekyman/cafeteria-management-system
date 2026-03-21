e1efe3f514adbd98530fb4914ad465437d9796e3
bekyman <bieky47@gmail.com>
2026-03-21 21:33:32 +0300

    allow requests from the frontend side

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,33 @@
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./config/database.js";
-import app from "./app.js";
+
+import reportRoutes from "./routes/reportRoutes.js";
 
 dotenv.config();
-connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/reports", reportRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
-
