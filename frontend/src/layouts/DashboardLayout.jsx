2691d0764252155a7922ef696a3f0f45be6fc882
bekyman <bieky47@gmail.com>
2026-03-27 18:59:41 +0300

    updating the files under layout subfolder inside src folder

---
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
+
+const DashboardLayout = () => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+
+      <div className="flex-1">
+        <Navbar />
+
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
