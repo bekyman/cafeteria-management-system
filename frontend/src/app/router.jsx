32d93d7b84ccfbbcccc82a4ad3fef7eac43b75df
bekyman <bieky47@gmail.com>
2026-03-29 17:35:26 +0300

    "add and modify some files"

---
+++ frontend/src/app/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import DashboardLayout from "../layouts/DashboardLayout";
+import AuthLayout from "../layouts/AuthLayout";
+
+import Dashboard from "../pages/Dashboard";
+import Inventory from "../pages/Inventory";
+import Orders from "../pages/Orders";
+import Menu from "../pages/Menu";
+import Kitchen from "../pages/Kitchen";
+import Reports from "../pages/Reports";
+import Login from "../pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    element: <DashboardLayout />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/inventory", element: <Inventory /> },
+      { path: "/orders", element: <Orders /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/kitchen", element: <Kitchen /> },
+      { path: "/reports", element: <Reports /> },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [{ path: "/login", element: <Login /> }],
+  },
+]);
