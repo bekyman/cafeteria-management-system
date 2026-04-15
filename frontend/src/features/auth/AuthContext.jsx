3d885aa19cf0d9d741ea328adbe6215361cedacf
bekyman <bieky47@gmail.com>
2026-04-15 16:29:21 +0300

    modify AuthContext.jsx

--- frontend/src/features/auth/AuthContext.jsx
+++ frontend/src/features/auth/AuthContext.jsx
@@ -1,20 +1,45 @@
-import { createContext, useState, useContext } from 'react';
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Will hold { id, username, role, token }
+  const [user, setUser] = useState(null);
 
-  const login = (userData) => {
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData, token) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(userData));
     setUser(userData);
   };
 
   const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+        isAuthenticated: !!user,
+        role: user?.role,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
