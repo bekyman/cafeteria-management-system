efff24d3060b76d42e0ba66af097718766113020
bekyman <bieky47@gmail.com>
2026-04-16 20:20:20 +0300

    Create roleRoutes.js

---
+++ backend/src/config/roleRoutes.js
@@ -0,0 +1,8 @@
+export const ROLE_DASHBOARD = {
+  ADMIN: "/admin",
+  MANAGER: "/manager",
+  CASHIER: "/pos",
+  KITCHEN: "/kitchen",
+  INVENTORY_MANAGER: "/inventory",
+  EMPLOYEE: "/orders",
+};
