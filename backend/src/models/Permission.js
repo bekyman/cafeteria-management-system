02b5f01225cf0d13cd74b3c7fd59ec06c8b6bce0
bekyman <bieky47@gmail.com>
2026-04-16 14:31:25 +0300

    modifications

--- backend/src/models/Permission.js
+++ backend/src/models/Permission.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const permissionSchema = new mongoose.Schema(
   {
-    // Unique permission key used by RBAC (ex: "sell_food")
     key: { type: String, required: true, unique: true, trim: true },
     description: { type: String, default: "" },
   },
