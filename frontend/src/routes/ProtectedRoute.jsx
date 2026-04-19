e93470f43e04a7a7051e425a8bf052a9e251e4ec
bekyman <bieky47@gmail.com>
2026-04-19 10:49:23 +0300

    Update ProtectedRoute.jsx

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,10 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ allowedRoles }) => {
+
   const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
 
   if (!token) {
     return <Navigate to="/login" />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    return <Navigate to="/" />;
   }
 
   return <Outlet />;
