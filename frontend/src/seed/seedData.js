a5b8b6adf6843ad572a2e38a006268d0075c5c01
bekyman <bieky47@gmail.com>
2026-04-22 10:38:09 +0300

    Update seedData.js

--- frontend/src/seed/seedData.js
+++ frontend/src/seed/seedData.js
@@ -1,23 +1,29 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import connectDB from "../config/database.js";
 import User from "../models/User.js";
 
 dotenv.config();
 
-await connectDB();
+const seed = async () => {
+  try {
+    await connectDB();
 
-await User.deleteMany();
+    await User.deleteMany();
 
-await User.create([
-  {
-    name: "System Admin",
-    email: "admin@moh.gov.et",
-    password: "123456",
-    role: "admin",
-  },
-]);
+    await User.create({
+      name: "System Admin",
+      email: "admin@moh.gov.et",
+      password: "123456",
+      role: "admin",
+    });
 
-console.log("✅ Database seeded");
+    console.log("✅ Database Seeded");
 
-process.exit();
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+seed();
