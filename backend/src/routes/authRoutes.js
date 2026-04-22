7903917a3e3fbc69a5c08f4b18ff8ef689104159
bekyman <bieky47@gmail.com>
2026-04-22 21:23:07 +0300

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { login, registerEmployee } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register/employee", registerEmployee);
 router.post("/login", login);
+router.post("/register", registerEmployee);
 
 export default router;
