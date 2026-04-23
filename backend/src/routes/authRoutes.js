ddbc12ef78a28dc5877ba84d35667cc794a4929c
bekyman <bieky47@gmail.com>
2026-04-23 15:25:36 +0300

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
 import { login, registerEmployee } from "../controllers/authController.js";
+import {
+  validateEmployeeRegister,
+  validateLogin,
+} from "../validators/authValidator.js";
+import validate from "../middleware/validate.js";
 
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/register", registerEmployee);
+router.post(
+  "/register/employee",
+  validateEmployeeRegister,
+  validate,
+  registerEmployee
+);
+
+router.post(
+  "/login",
+  validateLogin,
+  validate,
+  login
+);
 
 export default router;
